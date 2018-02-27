108549af008648386e3ac56dd9edc9c87c881b49
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-02-27 17:05:31 -0300

    Building rest api for customers and movies

---
+++ The_Complete_ASP.NET_MVC_5_Course/Vidly/Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie's name.")]
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime AddedDate { get; set; }
+
+        // [Min1Max20NumberStock]
+        public int NumberInStock { get; set; }
+
+        public byte GenreId { get; set; }
+    }
+}
