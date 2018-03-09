dedbf6c2b5d646de762665098453cd6e2f657511
Gabriel Miranda <gabrielmirandatt@gmail.com>
2018-03-09 13:56:28 -0300

    Adding study in performance

---
+++ The_Complete_ASP.NET_MVC_5_Course/Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m=> newRental.MovieIds.Constains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+        }
+    }
+}
